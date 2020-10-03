ae46a45c03ca8dbbf43e88fb06c240778b99be39
Artiom <artiom.filkevich@gmail.com>
2020-10-03 17:12:16 +0300

    feat: implement 'towel sort' function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  // return empty array if matrix is empty
+  if (matrix == undefined) { return [] }
+
+  // reverse every even array in matrix
+  for (let i = 1; i < matrix.length; ) {
+    matrix[i] = matrix[i].reverse();
+    i = i + 2;
+  }
+
+  // concat every sub-array into one array
+  matrix = matrix.flat(1);
+  return matrix;
 }
